1ab3e3b78ba6a1fb8dc2ff1b7719e6e54e39805e
Timo <timo.roskaposti@live.fi>
2014-03-08 00:33:52 +0200

    After testing seems that Z issue is hard to fix

--- dist/paper-full.js
+++ dist/paper-full.js
@@ -6399,7 +6399,7 @@ var PathItem = Item.extend({
 				  this.cubicCurveTo(new Point(segs[k], segs[k+1]), new Point(segs[k+2], segs[k+3]), new Point(segs[k+4], segs[k+5]));
 				break;
 			case 'z':
-				this.closePath(subpath, i);
+				this.closePath();
 				break;
 			}
 			previous = command;
@@ -6564,20 +6564,7 @@ var Path = PathItem.extend({
 		return this._closed;
 	},
 
-	setClosed: function(closed, firstI) {
-		if (this._closed != (closed = !!closed)) {
-			this._closed = closed;
-			if (this._curves) {
-				var length = this._curves.length = this._countCurves();
-				if (closed)
-					this._curves[length - 1] = new Curve(this,
-						this._segments[length - 1], this._segments[firstI]);
-			}
-			this._changed(5);
-		}
-	},
-
-	setClosed2: function(closed) {
+	setClosed: function(closed) {
 		if (this._closed != (closed = !!closed)) {
 			this._closed = closed;
 			if (this._curves) {
@@ -7593,16 +7580,7 @@ var Path = PathItem.extend({
 			}
 		},
 
-		closePath: function(firstI) {
-				var first = this._segments[firstI],
-			  last = this.getLastSegment();
-			if (first !== last && first._point.equals(last._point)) {
-				first.setHandleIn(last._handleIn);
-				last.remove();
-			}
-			this.setClosed(true, firstI);
-		},
-		closePath2: function(firstI, lastI) {
+		closePath: function(firstI, lastI) {
 			  var first = this.getFirstSegment(),
 			  last = this.getLastSegment();
 			if (first !== last && first._point.equals(last._point)) {
