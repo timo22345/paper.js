5c1d9c298d7f87f7a94fa8c200bc3169087328c6
Timo <timo.roskaposti@live.fi>
2014-03-08 00:04:57 +0200

    asdf

--- dist/paper-full.js
+++ dist/paper-full.js
@@ -6399,8 +6399,8 @@ var PathItem = Item.extend({
 				  this.cubicCurveTo(new Point(segs[k], segs[k+1]), new Point(segs[k+2], segs[k+3]), new Point(segs[k+4], segs[k+5]));
 				break;
 			case 'z':
-				this.closePath();
 				this.lineTo(current);
+				this.closePath();
 				break;
 			}
 			previous = command;
