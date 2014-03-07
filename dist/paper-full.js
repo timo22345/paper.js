e6616434be23ada82631f670dac3086af3d58f4e
Timo <timo.roskaposti@live.fi>
2014-03-07 23:58:26 +0200

    sadfsddf

--- dist/paper-full.js
+++ dist/paper-full.js
@@ -6391,7 +6391,6 @@ var PathItem = Item.extend({
 				}
 				break;
 			case 'a':
-
 				var segs = a2c(current.x, current.y, +coords[0], +coords[1], +coords[2], +coords[3], +coords[4], !relative? +coords[5]:+coords[5]+current.x, !relative? +coords[6]:+coords[6]+current.y);
 	      current.x = !relative? +coords[5]: +coords[5]+current.x;
 	      current.y = !relative? +coords[6]: +coords[6]+current.y;
@@ -6404,6 +6403,7 @@ var PathItem = Item.extend({
 				//this.closePath();
 				this.lineTo(subpath);
 				current = subpath;
+				control = current;
 				break;
 			}
 			previous = command;
