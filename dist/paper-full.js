e89e56182594b08e93874c115f90e8974391bb18
Timo <timo.roskaposti@live.fi>
2014-03-09 20:55:15 +0200

    sfdfdggf

--- dist/paper-full.js
+++ dist/paper-full.js
@@ -6398,7 +6398,7 @@ var PathItem = Item.extend({
 
         for (var k = 0, klen = segs.length; k < klen; k += 6)
 				  this.cubicCurveTo(new Point(segs[k], segs[k+1]), new Point(segs[k+2], segs[k+3]), new Point(segs[k+4], segs[k+5]));
-        k--;
+        //k--;
         current = new Point(segs[k+4], segs[k+5]);
 				break;
 			case 'z':
